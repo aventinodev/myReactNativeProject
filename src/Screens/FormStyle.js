00006c2c0c13670b5266ad22a48aee723907e53a
Olena Bondar <olena.bondar1060@gmail.com>
2023-03-28 16:27:05 +0300

    Create FormStyle.js

---
+++ src/Screens/FormStyle.js
@@ -0,0 +1,72 @@
+import { StyleSheet } from "react-native";
+
+export const stylesForm = StyleSheet.create({
+  title: {
+    color: "#20232a",
+    textAlign: "center",
+    fontSize: 30,
+    fontWeight: "bold",
+    marginBottom: 32,
+    // fontFamily: "Roboto-Medium",
+  },
+  container: {
+    width: "100%",
+    backgroundColor: "#fff",
+    borderTopLeftRadius: 25,
+    borderTopRightRadius: 25,
+    alignItems: "center",
+  },
+  form: {
+    position: "relative",
+    paddingTop: 92,
+
+    borderTopRightRadius: 25,
+    borderTopLeftRadius: 25,
+    // paddingHorizontal: 8,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  input: {
+    width: "100%",
+    height: 44,
+    padding: 10,
+    borderWidth: 1,
+    // borderColor: "#E8E8E8",
+    borderRadius: 8,
+    marginBottom: 16,
+    backgroundColor: "#F6F6F6",
+  },
+  button: {
+    width: 300,
+    borderColor: "#FF6C00",
+    borderRadius: "50%",
+    paddingVertical: 16,
+    backgroundColor: "#FF6C00",
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 27,
+    marginBottom: 16,
+  },
+  text: {
+    lineHeight: 19,
+    textAlign: "center",
+    // fontFamily: "Roboto-Medium",
+  },
+  wrap: {
+    position: "absolute",
+    top: -60,
+    left: "50%",
+    transform: [{ translateX: -50 }],
+    width: 120,
+    height: 120,
+    borderRadius: 16,
+
+    backgroundColor: "#F6F6F6",
+  },
+  btnAdd: {
+    position: "absolute",
+    right: -12.5,
+    bottom: 14,
+  },
+});
